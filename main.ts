1b8fe1f0589df84cf8f1b2680d603fbd31f5a23b
tinnist <82404492+tinnist@users.noreply.github.com>
2021-04-14 13:28:39 -0400

    added things from task

--- main.ts
+++ main.ts
@@ -1,7 +1,26 @@
+controller.up.onEvent(ControllerButtonEvent.Pressed, function () {
+    flipHorizontal()
+})
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    flipVertical()
+})
+controller.left.onEvent(ControllerButtonEvent.Pressed, function () {
+    flipHorizontal()
+})
+controller.right.onEvent(ControllerButtonEvent.Pressed, function () {
+    flipHorizontal()
+})
+function flipVertical () {
+    mySprite.image.flipY()
+    pause(200)
+}
 function flipHorizontal () {
     mySprite.image.flipX()
     pause(200)
 }
+controller.down.onEvent(ControllerButtonEvent.Pressed, function () {
+    flipHorizontal()
+})
 let mySprite: Sprite = null
 mySprite = sprites.create(img`
     . . . . . . . . . . . . . . . . 
@@ -21,3 +40,4 @@ mySprite = sprites.create(img`
     . . . . . . . . . . . . . . . . 
     . . . . . . . . . . . . . . . . 
     `, SpriteKind.Player)
+scene.setBackgroundColor(6)
